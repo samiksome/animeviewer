1c191811232163cc708a75260333b163098d7e7f
Samik Some <decoy.samik@gmail.com>
2015-12-16 15:00:04 +0530

    Changed input to raw_input

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import gatherer
 
-searchterms = input("search title ")
+searchterms = raw_input("search title ")
 matchlist = gatherer.search("anime-titles.xml",searchterms)
 for i in range(len(matchlist)):
 	print str(i+1)+": "+matchlist[i][1]
